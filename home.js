e698922555e99a23a6c917663d6f42e40709fa20
Davbytes <162809162+Davbytes@users.noreply.github.com>
2024-09-30 20:45:55 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,12 @@
+function toggleForms() {
+  var Signin = document.getElementById('Signin');
+  var Signin = document.getElementById('Signin');
+  
+  if (Login.style.display === "none") {
+      Login.style.display = "block";
+      Signin.style.display = "none";
+  } else {
+      Login.style.display = "none";
+      Signin.style.display = "block";
+  }
+}
